574ca65378c1961cccef1b51182ecb7b952de626
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-13 14:38:22 +0800

    change the font-color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,42 +7,42 @@ export default function Header() {
       <div className="flex items-center max-w-6xl mx-auto p-3">
         <Link to="/" className="absolute left-0 flex-shrink-0">
           <h1 className="font-bold text-lg sm:text-xl">
-            <span className="text-slate-950">ProCapture</span>
+            <span className="text-black">ProCapture</span>
           </h1>
         </Link>
         <div className="flex-grow flex justify-center">
           <ul className="inline-flex gap-20">
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Home
             </li>
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Shop
             </li>
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Deals
             </li>
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Brands
             </li>
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Blog
             </li>
-            <li className="hidden sm:inline text-slate-950 hover:shadow-lg transition-shadow duration-200 font-semibold">
+            <li className="hidden sm:inline text-black hover:shadow-lg transition-shadow duration-200 font-semibold">
               Support
             </li>
           </ul>
         </div>
         <div className="absolute right-0 flex gap-6 items-center pr-3">
           <FaSearch
-            className="text-slate-950 hover:text-gray-500 cursor-pointer"
+            className="text-black hover:text-gray-500 cursor-pointer"
             size={20}
           />
           <FaUserAlt
-            className="text-slate-950 hover:text-gray-500 cursor-pointer"
+            className="text-black hover:text-gray-500 cursor-pointer"
             size={20}
           />
           <FaShoppingCart
-            className="text-slate-950 hover:text-gray-500 cursor-pointer"
+            className="text-black hover:text-gray-500 cursor-pointer"
             size={20}
           />
         </div>
