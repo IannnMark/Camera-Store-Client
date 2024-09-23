be6a2ed4060fa26daae8c9a4c9257f717270220c
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-23 13:06:09 +0800

    configure the store in the mainjsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import ReactDom from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { Provider } from "react-redux";
+import { store, persistore } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDom.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistore={persistore}>
+      <App />
+    </PersistGate>
+  </Provider>
 );
