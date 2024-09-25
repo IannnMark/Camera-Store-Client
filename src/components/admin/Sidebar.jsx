904dc287eb5e8e4942b2d146fbc5b9d2f10dc64e
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-25 14:34:41 +0800

    sidebar

---
+++ src/components/admin/Sidebar.jsx
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+export default function Sidebar() {
+  return (
+    <div className="h-screen w-40 bg-black text-white flex flex-col justify-between">
+      <nav className="flex flex-col space-y-3 p-6">
+        <Link className="hover:bg-gray-700 py-2 px-3 rounded font-semibold">
+          Dashboard
+        </Link>
+        <Link
+          to="/products"
+          className="hover:bg-gray-700 py-2 px-3 rounded font-semibold"
+        >
+          Products
+        </Link>
+        <Link
+          to="/users"
+          className="hover:bg-gray-700 py-2 px-3 rounded font-semibold"
+        >
+          Users
+        </Link>
+      </nav>
+    </div>
+  );
+}
