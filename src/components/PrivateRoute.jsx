bde5f9db760c0f847f425c4e9e82ddfcad58baba
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-25 13:29:18 +0800

    modify the Private Route to include the admin logic

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,7 +1,27 @@
 import { useSelector } from "react-redux";
 import { Outlet, Navigate } from "react-router-dom";
 
-export default function PrivateRoute() {
+import PropTypes from "prop-types";
+
+export default function PrivateRoute({ isAdmin = false }) {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? <Outlet /> : <Navigate to="/sign-in" />;
+
+  //if not authenticated, redirect to sign-in
+  if (!currentUser) {
+    return <Navigate to="/sign-in" />;
+  }
+
+  // if isAdmin is true and the current user is not an admin, redirect to home page
+  if (isAdmin && currentUser.role !== "admin") {
+    return <Navigate to="/" />;
+  }
+
+  //Otherwise, allow access to the protected route
+  return <Outlet />;
 }
+
+PrivateRoute.propTypes = {
+  isAdmin: PropTypes.shape({
+    userRef: PropTypes.string.isRequired,
+  }).isRequired,
+};
