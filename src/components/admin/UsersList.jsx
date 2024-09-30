0c66722e19bd5e8bdf78a0a0abb479c5d448f54e
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-30 16:01:32 +0800

    change the background color

---
+++ src/components/admin/UsersList.jsx
@@ -0,0 +1,75 @@
+import { useState, useEffect } from "react";
+// import Sidebar from "./Sidebar";
+
+export default function UsersList() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const fetchUsers = async () => {
+    try {
+      const res = await fetch("/api/user/admin/users");
+      const data = await res.json();
+      console.log(data);
+
+      setUsers(data.users || []);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching Users", error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  if (loading) {
+    return <p className="text-center text-white text-xl">Loading Users...</p>;
+  }
+
+  return (
+    <div className="container mx-auto p-6">
+      <h1 className="text-3xl text-black font-semibold text-center">
+        Users List
+      </h1>
+      <div className="overflow-x-auto my-7">
+        <table className="min-w-full bg-gray-100 text-black">
+          <thead>
+            <tr className="w-full border-b border-white">
+              <th className="py-3 px-6 text-left">Username</th>
+              <th className="py-3 px-6 text-left">Email</th>
+              <th className="py-3 px-6 text-left">Avatar</th>
+              <th className="py-3 px-6 text-left">Role</th>
+              <th className="py-3 px-6 text-left">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user) => (
+              <tr key={user._id} className="border-b border-white">
+                <td className="py-3 px-6">
+                  <img
+                    src={
+                      user.avatar ||
+                      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTu9zuWJ0xU19Mgk0dNFnl2KIc8E9Ch0zhfCg&s"
+                    }
+                    alt={user.username}
+                    className="w-10 h-10 object-cover"
+                  />
+                </td>
+                <td className="py-3 px-6 text-black font-normal">
+                  {user.username}
+                </td>
+                <td className="py-3 px-6 text-black font-normal">
+                  {user.email}
+                </td>
+                <td className="py-3 px-6 text-black font-normal">
+                  {user.role}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
