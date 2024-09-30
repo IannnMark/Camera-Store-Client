1fe3a25ae705c6684f3520a10f99484808b7ddbd
ianmark8 <ianmarkmorga07@gmail.com>
2024-09-30 19:24:08 +0800

    add admin delete user

--- src/components/admin/UsersList.jsx
+++ src/components/admin/UsersList.jsx
@@ -27,9 +27,31 @@ export default function UsersList() {
     return <p className="text-center text-white text-xl">Loading Users...</p>;
   }
 
+  const handleDeleteUser = async (userId) => {
+    const confirmDelete = window.confirm(
+      "are you sure you want to delete this user?"
+    );
+    if (confirmDelete) {
+      try {
+        const res = await fetch(`/api/user/admin/delete/${userId}`, {
+          method: "DELETE",
+        });
+        const data = await res.json();
+        if (data.success === false) {
+          console.log(data.message);
+          return;
+        }
+        alert("User deleted successfully");
+        window.location.reload();
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+  };
+
   return (
     <div className="container mx-auto p-6">
-      <h1 className="text-3xl text-black font-semibold text-center">
+      <h1 className="text-3xl text-black font-semibold mb-4 text-center">
         Users List
       </h1>
       <div className="overflow-x-auto my-7">
@@ -64,6 +86,15 @@ export default function UsersList() {
                 </td>
                 <td className="py-3 px-6 text-black font-normal">
                   {user.role}
+                </td>
+                <td className="py-3 px-6 text-black font-normal">
+                  <button className="text-green-800 uppercase">Edit</button>
+                  <button
+                    className="text-red-800 uppercase ml-6"
+                    onClick={() => handleDeleteUser(user._id)}
+                  >
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}
