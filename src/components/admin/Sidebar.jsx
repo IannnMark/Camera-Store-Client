140494d3eec25f9108c381b74411c6fbeaa4a0dc
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-02 09:43:43 +0800

    remove declaration

--- src/components/admin/Sidebar.jsx
+++ src/components/admin/Sidebar.jsx
@@ -13,7 +13,7 @@ import { Link } from "react-router-dom";
 const SidebarContext = createContext();
 
 export default function Sidebar() {
-  const [expanded, setExpanded] = useState(true);
+  const [expanded] = useState(true);
   const [open, setOpen] = useState(false);
   const [showProductsSubmenu, setShowProductsSubmenu] = useState(false);
   const sidebarRef = useRef(null);
