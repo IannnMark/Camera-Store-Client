1898a5acef1145711b00d2f8935e1cd2b10c74bb
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-05 16:23:34 +0800

    change the gcash image upload

--- src/pages/cart/Payment.jsx
+++ src/pages/cart/Payment.jsx
@@ -1,4 +1,5 @@
-import React, { Fragment, useState } from "react";
+import { Fragment, useState } from "react";
+import { app } from ".././../firebase";
 import { useNavigate } from "react-router-dom";
 import CheckoutSteps from "./CheckoutSteps";
 import { useSelector, useDispatch } from "react-redux";
@@ -9,20 +10,29 @@ import {
   orderCreateSuccess,
   orderCreateFail,
 } from "../../redux/order/orderSlice";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 
 export default function Payment() {
   let navigate = useNavigate();
   const dispatch = useDispatch();
   const { cartItems } = useSelector((state) => state.cart);
-
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    screenShot: [],
+  });
   const [paymentInfo, setPaymentInfo] = useState("Cash");
   const [referenceNum, setReferenceNum] = useState("");
   const [referenceNumError, setReferenceNumError] = useState("");
-  const [screenShot, setScreenShot] = useState([]);
-  const [screenShotPreview, setScreenShotPreview] = useState([]);
+  const [imageUploadError, setImageUploadError] = useState(false);
+  const [uploading, setUploading] = useState(false);
 
   const orderInfo = JSON.parse(sessionStorage.getItem("orderInfo")) || {};
-  const totalPrice = parseFloat(orderInfo.totalPrice) || 0; // Ensure totalPrice is a number
+  const totalPrice = parseFloat(orderInfo.totalPrice) || 0;
 
   const validateReferenceNum = () => {
     if (paymentInfo === "GCash" && referenceNum.length !== 13) {
@@ -34,75 +44,68 @@ export default function Payment() {
     }
   };
 
-  const handleScreenshotChange = (e) => {
-    const files = Array.from(e.target.files);
-    setScreenShotPreview([]);
-    setScreenShot([]);
+  const handleImageSubmit = async (e) => {
+    e.preventDefault();
 
-    files.forEach((file) => {
-      const reader = new FileReader();
-      reader.onload = () => {
-        if (reader.readyState === 2) {
-          setScreenShotPreview((prev) => [...prev, reader.result]);
-          setScreenShot((prev) => [...prev, reader.result]);
+    if (files.length > 0 && files.length + formData.screenShot.length < 7) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData((prevState) => ({
+            ...prevState,
+            screenShot: prevState.screenShot.concat(urls),
+          }));
+          setImageUploadError(false);
+          setUploading(false);
+        })
+        .catch((error) => {
+          console.log("Image upload error", error);
+          setImageUploadError("Image upload failed (2 mb max per image)");
+        });
+    } else {
+      setImageUploadError("You can only upload 6 images per screenshot");
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log(`Upload is ${progress}% done`);
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
         }
-      };
-      reader.readAsDataURL(file);
+      );
     });
   };
 
-  //   const submitHandler = async (e) => {
-  //     e.preventDefault();
-  //     document.querySelector("#pay_btn").disabled = true;
-
-  //     if (!validateReferenceNum()) {
-  //       document.querySelector("#pay_btn").disabled = false;
-  //       return;
-  //     }
-
-  //     if (paymentInfo === "GCash" && screenShot.length === 0) {
-  //       toast.error("Screenshot is required for GCash payment");
-  //       document.querySelector("#pay_btn").disabled = false;
-  //       return;
-  //     }
-
-  //     const orderData = {
-  //       orderItems: cartItems,
-  //       paymentInfo,
-  //       referenceNumber: referenceNum,
-  //       screenshots: screenShot,
-  //       itemsPrice: orderInfo.itemsPrice,
-  //       shippingPrice: orderInfo.shippingPrice,
-  //       totalPrice,
-  //     };
-
-  //     try {
-  //       dispatch(orderCreateRequest());
-
-  //       const response = await fetch(`/api/order/new`, {
-  //         method: "POST",
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //         },
-  //         body: JSON.stringify(orderData),
-  //       });
-
-  //       const data = await response.json();
-
-  //       if (response.ok) {
-  //         dispatch(orderCreateSuccess(data.order));
-  //         dispatch(clearCart());
-  //         navigate("/success");
-  //       } else {
-  //         throw new Error(data.message || "Failed to create order");
-  //       }
-  //     } catch (error) {
-  //       toast.error(error.message);
-  //       dispatch(orderCreateFail(error.message));
-  //     } finally {
-  //       document.querySelector("#pay_btn").disabled = false;
-  //     }
-  //   };
+  const handleRemoveImage = (index) => {
+    setFormData({
+      ...formData,
+      screenShot: formData.screenShot.filter((_, i) => i !== index),
+    });
+  };
 
   const submitHandler = async (e) => {
     e.preventDefault();
@@ -113,7 +116,7 @@ export default function Payment() {
       return;
     }
 
-    if (paymentInfo === "GCash" && screenShot.length === 0) {
+    if (paymentInfo === "GCash" && formData.screenShot.length === 0) {
       toast.error("Screenshot is required for GCash payment");
       document.querySelector("#pay_btn").disabled = false;
       return;
@@ -121,7 +124,7 @@ export default function Payment() {
 
     const orderData = {
       orderItems: cartItems.map((item) => ({
-        product: item._id, // Ensure product field is included
+        product: item._id,
         modelName: item.modelName,
         quantity: item.quantity,
         price: item.price,
@@ -129,7 +132,7 @@ export default function Payment() {
       })),
       paymentInfo,
       referenceNumber: referenceNum,
-      screenshots: screenShot,
+      screenShot: formData.screenShot, 
       itemsPrice: orderInfo.itemsPrice,
       shippingPrice: orderInfo.shippingPrice,
       totalPrice: orderInfo.totalPrice,
@@ -169,6 +172,7 @@ export default function Payment() {
       <div className="flex justify-center items-center py-10">
         <div className="w-full max-w-lg bg-white p-8 rounded-lg shadow-lg">
           <form onSubmit={submitHandler} className="space-y-6">
+            {/* Payment Method */}
             <div className="mb-6">
               <label className="block text-gray-700 text-sm font-bold mb-2">
                 Select Payment Method:
@@ -228,40 +232,59 @@ export default function Payment() {
                   )}
                 </div>
 
-                <div className="mt-6">
-                  <label className="block text-gray-700 text-sm font-bold mb-2">
-                    Upload Screenshot:
-                  </label>
+                <div className="flex gap-4">
                   <input
+                    onChange={(e) => setFiles(e.target.files)}
+                    className="p-3 border border-gray-300 rounded-full"
                     type="file"
-                    className="block w-full text-sm text-gray-500 border border-gray-300 rounded-lg cursor-pointer"
-                    onChange={handleScreenshotChange}
+                    id="images"
+                    accept="image/*"
                     multiple
                   />
-                  <div className="flex mt-4 space-x-2">
-                    {screenShotPreview.map((img) => (
-                      <img
-                        src={img}
-                        alt="Screenshot Preview"
-                        key={img}
-                        className="w-16 h-16 rounded-md object-cover"
-                      />
-                    ))}
-                  </div>
+                  <button
+                    type="button"
+                    disabled={uploading}
+                    onClick={handleImageSubmit}
+                    className="p-3 text-green-800 border border-green-600 rounded uppercase
+                  hover:shadow-lg disabled:opacity-80"
+                  >
+                    {uploading ? "Uploading" : "Upload"}
+                  </button>
                 </div>
-                
+                <p className="text-red-800 text-sm">
+                  {imageUploadError && imageUploadError}
+                </p>
+                {formData.screenShot.length > 0 &&
+                  formData.screenShot.map((url, index) => (
+                    <div
+                      key={url}
+                      className="flex justify-between p-3 border items-center"
+                    >
+                      <img
+                        src={url}
+                        alt="Uploaded screenshot"
+                        className="w-20 h-20 object-contain rounded-lg"
+                      />
+                      <button
+                        type="button"
+                        onClick={() => handleRemoveImage(index)}
+                        className="p-2 text-xs text-red-700 rounded-full
+                    hover:shadow-sm hover:bg-red-200"
+                      >
+                        Remove
+                      </button>
+                    </div>
+                  ))}
               </>
             )}
 
-            <div className="mt-6">
-              <button
-                id="pay_btn"
-                type="submit"
-                className="w-full bg-indigo-600 text-white py-3 rounded-md text-lg font-semibold"
-              >
-                ORDER - ₱{totalPrice.toFixed(2)}
-              </button>
-            </div>
+            <button
+              type="submit"
+              id="pay_btn"
+              className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            >
+              Pay ₱{totalPrice.toFixed(2)}
+            </button>
           </form>
         </div>
       </div>
