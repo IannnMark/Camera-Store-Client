269b1b75890867ea29f17a893c5da042b80f3caa
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-11 13:47:38 +0800

    add styling to the single brand

--- src/pages/BrandProduct.jsx
+++ src/pages/BrandProduct.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { addItemToCart } from "../redux/cart/cartSlice";
+import { Link } from "react-router-dom";
 
 import Footer from "../components/Footer";
 
@@ -67,51 +68,54 @@ export default function BrandProducts() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100">
-      <section className="container mx-auto py-10">
-        <h2 className="text-3xl text-center font-semibold mb-8">
-          Products for {brandId} Brand
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 px-4">
-          {products.map((product) => (
-            <div
-              key={product._id}
-              className="bg-white p-6 rounded-lg shadow-md"
-            >
+    <div className="min-h-screen bg-gray-300">
+      <h2 className="text-3xl text-center font-semibold mb-8">
+        Products for {brandId} Brand
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-20 px-4">
+        {products.map((product) => (
+          <div
+            key={product._id}
+            className="bg-gray-300 shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px] ml-3"
+          >
+            <Link to={`/product/${product._id}`}>
               <img
                 src={product.imageUrls[0]}
-                alt={product.modelName}
+                alt="Product Cover"
                 className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
               />
-              <h3 className="text-xl font-bold text-gray-800 mb-2 hover:scale-105 transition-scale duration-300">
-                {product.modelName}
-              </h3>
-              <p className="text-gray-700 font-semibold mt-2 hover:scale-105 transition-scale duration-300">
-                $
-                {product.offer && product.discountPrice
-                  ? product.discountPrice.toLocaleString("en-US")
-                  : product.regularPrice
-                  ? product.regularPrice.toLocaleString("en-US")
-                  : "N/A"}{" "}
-                USD
-              </p>
-              <button
-                onClick={() => handleAddToCart(product)}
-                className="text-xl text-white bg-black rounded-md hover:bg-gray-800 px-2 mt-4"
-              >
-                Add to Cart
-              </button>
-              {product.stock <= 0 && (
-                <p className="text-red-500 text-center mt-2">
-                  Sorry, this product is out of stock!
+              <div className="p-3 flex flex-col gap-2 hover:scale-105 transition-scale duration-300">
+                <p className="truncate text-lg font-semibold text-black">
+                  {product.modelName}
                 </p>
-              )}
-            </div>
-          ))}
-        </div>
-      </section>
-
-      <Footer />
+                <p className="text-gray-700 font-semibold mt-2">
+                  $
+                  {product.offer && product.discountPrice
+                    ? product.discountPrice.toLocaleString("en-US")
+                    : product.regularPrice
+                    ? product.regularPrice.toLocaleString("en-US")
+                    : "N/A"}{" "}
+                  USD
+                </p>
+              </div>
+            </Link>
+            <button
+              onClick={() => handleAddToCart(product)}
+              className="text-xl text-white bg-black rounded-md ml-28 hover:bg-gray-800 px-2"
+            >
+              Add to Cart
+            </button>
+            {product.stock <= 0 && (
+              <p className="text-red-500 text-center mt-2">
+                Sorry, this product is out of stock!
+              </p>
+            )}
+          </div>
+        ))}
+      </div>
+      <div className="my-10">
+        <Footer />
+      </div>
     </div>
   );
 }
