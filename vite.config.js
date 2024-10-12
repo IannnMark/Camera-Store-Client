d2b55afe4f4dbc85693ec0f553c736d64c8b6180
ianmark8 <ianmarkmorga07@gmail.com>
2024-10-12 18:11:10 +0800

    use the localhost proxy

--- vite.config.js
+++ vite.config.js
@@ -4,33 +4,33 @@ import react from '@vitejs/plugin-react';
 
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   server: {
-//     proxy: {
-//       '/api': {
-//         target: "http://localhost:3000",
-//         secure: false,
-//       },
-//     },
-//   },
-//   plugins: [react()],
-// })
+export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000",
+        secure: false,
+      },
+    },
+  },
+  plugins: [react()],
+})
 
 
 
 
 //deployed proxy
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: "https://camera-store-api.vercel.app",
-        changeOrigin: true,
-        secure: true,
-      },
-    },
-  },
-  plugins: [react()],
-})
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: "https://camera-store-api.vercel.app",
+//         changeOrigin: true,
+//         secure: true,
+//       },
+//     },
+//   },
+//   plugins: [react()],
+// })
 
